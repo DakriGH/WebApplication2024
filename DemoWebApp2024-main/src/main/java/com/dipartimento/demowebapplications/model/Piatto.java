e16a3aa89e09c71c80b72b916d22df293cc77feb
DakriGH <favaro.christian01@gmail.com>
2024-11-21 22:18:53 +0100

    Add files via upload

---
+++ DemoWebApp2024-main/src/main/java/com/dipartimento/demowebapplications/model/Piatto.java
@@ -0,0 +1,17 @@
+package com.dipartimento.demowebapplications.model;
+
+import lombok.*;
+import java.util.List;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Piatto {
+    private String nome;
+    private String ingredienti;
+    protected List<Ristorante> ristoranti;
+}
+
